ecb9ae426fca1af2b574329cfdae80a7f9374345
Kimdonghwan <kimdonghwan913@gmail.com>
2025-04-08 15:10:16 +0900

    chore: PR 템플릿 작성

--- src/main/java/com/bizket/BizketApplication.java
+++ src/main/java/com/bizket/BizketApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BizketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BizketApplication.class, args);
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BizketApplication.class, args);
+    }
 
 }
