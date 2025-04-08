2b16349b7e193b8633c73fce9cffbd4f8444f7ad
Kimdonghwan <kimdonghwan913@gmail.com>
2025-04-08 11:51:53 +0900

    chore: 초기 설정

---
+++ src/test/java/com/bizket/BizketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
