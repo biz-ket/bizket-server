39c709d18d62b1cbfdac2a8e0c2119b21d6fbdab
YinaePark <dlsoso1@unist.ac.kr>
2025-04-09 23:23:10 +0900

    feat: member 도메인, 레포지토리

---
+++ src/main/java/com/bizket/member/domain/Member.java
@@ -0,0 +1,31 @@
+package com.bizket.member.domain;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String nickname;
+
+    public Member(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+}
