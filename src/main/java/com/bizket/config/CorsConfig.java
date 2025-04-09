7a4c14ca50ebc7b0dcb4f1b4f00d1a737827a200
YinaePark <dlsoso1@unist.ac.kr>
2025-04-09 23:32:32 +0900

    feat: CORS Config Authorization 헤더 노출 설정

---
+++ src/main/java/com/bizket/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.bizket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        // 응답 헤더에 Authorization을 노출하도록 설정 (필요한 경우)
+        configuration.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
