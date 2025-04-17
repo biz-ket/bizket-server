c36d57ba08f2d1362520c140891a750588d981c0
YinaePark <dlsoso1@unist.ac.kr>
2025-04-17 21:33:58 +0900

    feat: member 레포지토리

---
+++ src/main/java/com/bizket/common/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.bizket.common.member.repository;
+
+import com.bizket.common.member.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByProviderIdAndOauth2Provider(String providerId, String oauth2Provider);
+
+}
