ca15bc4357a738607b16615dd125dafd4aa15dfc
YinaePark <dlsoso1@unist.ac.kr>
2025-04-18 23:45:58 +0900

    style: 불필요 어노테이션 삭제

--- src/main/java/com/bizket/common/member/domain/Member.java
+++ src/main/java/com/bizket/common/member/domain/Member.java
@@ -11,12 +11,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "member")
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
