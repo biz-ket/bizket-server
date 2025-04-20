0ede8f5f4989e3b945faf7ff4c5f182ffb9154bd
Kimdonghwan <kimdonghwan913@gmail.com>
2025-04-20 14:04:39 +0900

    feat: wait-for-it.sh 스크립트 추가

---
+++ wait-for-it.sh
@@ -0,0 +1,13 @@
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until nc -z ${host%:*} ${host##*:}; do
+  >&2 echo "Wait-for-it: $host is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Wait-for-it: $host is up - executing command"
+exec $cmd
