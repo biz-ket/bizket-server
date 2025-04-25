ca3eff7302ad34d97ff61865bb5732618eca7a66
YinaePark <dlsoso1@unist.ac.kr>
2025-04-26 02:34:18 +0900

    fix: security config whitelist

--- src/main/java/com/bizket/auth/config/SecurityConstant.java
+++ src/main/java/com/bizket/auth/config/SecurityConstant.java
@@ -3,15 +3,11 @@ package com.bizket.auth.config;
 public final class SecurityConstant {
 
     public static final String[] WHITE_LIST = {
-        "/api/oauth2/authorization/**",
-        "/api/login/oauth2/code/**",
-        "/api/auth/reissue",
-        "/api/auth/**",
         "/oauth2/**",
         "/favicon.ico",
         "/error",
         "/auth/instagram/login",
         "/auth/instagram/exchange",
-        "/test"
+        "/login/callback"
     };
 }
