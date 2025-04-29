2f361dc4aa813826f6530319c9daf371e1df1e04
YinaePark <dlsoso1@unist.ac.kr>
2025-04-30 02:09:17 +0900

    feat: member entity 수정

--- src/main/java/com/bizket/common/member/domain/Member.java
+++ src/main/java/com/bizket/common/member/domain/Member.java
@@ -1,11 +1,18 @@
 package com.bizket.common.member.domain;
 
+import static jakarta.persistence.FetchType.*;
+
 import com.bizket.common.model.BaseEntity;
+import com.bizket.insight.domain.BusinessPlace;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,8 +45,24 @@ public class Member extends BaseEntity {
     @Column(name = "provider_id", nullable = false)
     private String providerId;
 
-    @Column(name = "business_place_id")
-    private Long businessPlaceId;
+    @Column(name = "instagram_account_id", length = 255)
+    private String instagramAccountId;
+
+    @Column(name = "threads_account_id", length = 255)
+    private String threadsAccountId;
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "business_place_id")
+    private BusinessPlace businessPlace;
+
+    public void updateBusinessPlace(BusinessPlace businessPlace) {
+        this.businessPlace = businessPlace;
+    }
+
+    public void updateSnsAccount(String instagramAccountId, String threadsAccountId) {
+        this.instagramAccountId = instagramAccountId;
+        this.threadsAccountId = threadsAccountId;
+    }
 
     @Column(name = "is_deleted", columnDefinition = "BOOLEAN DEFAULT FALSE")
     private Boolean isDeleted;
