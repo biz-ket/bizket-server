283366104101a02eb9ddba7bc1683b576d4edad7
johoon030 <142702766+hoonjo123@users.noreply.github.com>
2025-05-05 00:44:48 +0900

    chore: ci-cd (#92)

--- src/test/java/com/bizket/BizketApplicationTests.java
+++ src/test/java/com/bizket/BizketApplicationTests.java
@@ -1,21 +1,10 @@
 package com.bizket;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-@EnableAutoConfiguration(
-		exclude = {
-				DataSourceAutoConfiguration.class,
-				DataSourceTransactionManagerAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
-		}
-)
 class BizketApplicationTests {
 
 	@Test
